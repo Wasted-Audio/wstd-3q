221bbd093b8a37bdd3e9baa806ca58a8e53e53ac
dreamer <dreamer@puikheid.nl>
2025-12-08 10:55:00 +0100

    auto set constraints; cleanup unused ResizeHandle

--- override/HeavyDPF_WSTD_3Q_UI.cpp
+++ override/HeavyDPF_WSTD_3Q_UI.cpp
@@ -19,8 +19,6 @@ class ImGuiPluginUI : public UI
     float flow = 0.0f;
     float fmid = 0.0f;
 
-    ResizeHandle fResizeHandle;
-
     // ----------------------------------------------------------------------------------------------------------------
 
 public:
@@ -29,11 +27,8 @@ public:
       The UI should be initialized to a default state that matches the plugin side.
     */
     ImGuiPluginUI()
-        : UI(DISTRHO_UI_DEFAULT_WIDTH, DISTRHO_UI_DEFAULT_HEIGHT, true),
-          fResizeHandle(this)
+        : UI(DISTRHO_UI_DEFAULT_WIDTH, DISTRHO_UI_DEFAULT_HEIGHT)
     {
-        setGeometryConstraints(DISTRHO_UI_DEFAULT_WIDTH, DISTRHO_UI_DEFAULT_HEIGHT, true);
-
         ImGuiIO& io(ImGui::GetIO());
 
         ImFontConfig fc;
@@ -46,8 +41,6 @@ public:
         io.Fonts->AddFontFromMemoryCompressedTTF((void*)veramobd_compressed_data, veramobd_compressed_size, 21.0f * getScaleFactor(), &fc);
         io.Fonts->Build();
         io.FontDefault = io.Fonts->Fonts[1];
-
-        fResizeHandle.hide();
     }
 
 protected:
